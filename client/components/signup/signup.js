9272ee7a6d046d98da9c56dc024502aaf04fca3b
aiduc0502 <batnapwantai_kissemlancuoi1@yahoo.com>
2017-07-05 20:11:14 +0700

    fixed confict (#3)

--- client/components/signup/signup.js
+++ client/components/signup/signup.js
@@ -1,14 +1,5 @@
 import React from "react"
-<<<<<<< HEAD
 
-class SignUp extends React.Component {
-  render() {
-    return (
-      <h1> This is sign up page </h1>
-    )
-  }
-}
-=======
 import SignUpForm from "./../signupform/signup-form"
 class SignUp extends React.Component {
   render() {
@@ -25,4 +16,3 @@ class SignUp extends React.Component {
   }
 }
 export default SignUp
->>>>>>> f96b9279f399988654ac14d16642cf0fe766d17c
