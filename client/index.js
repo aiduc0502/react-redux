c5ba03f55ab6a8d0e607cf7c4a80c28dd17e13b2
duc.vuong <duc.vuong@sutrixsolutions.com>
2017-07-05 20:11:08 +0700

    fixed confict

--- client/index.js
+++ client/index.js
@@ -1,11 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
 import App from "./views/app";
-<<<<<<< HEAD
-import Routes from './routes';
-render(<Routes/>, document.getElementById('app'));
-=======
+
 import Routes from './views/routes';
 
 render(<Routes />, document.getElementById('app'));
->>>>>>> f96b9279f399988654ac14d16642cf0fe766d17c
