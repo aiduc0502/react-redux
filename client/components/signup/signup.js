3b1fb3619f9f3a4b994043ce59006a19e8be304b
aiduc0502 <batnapwantai_kissemlancuoi1@yahoo.com>
2017-07-06 18:15:24 +0700

    add component, axios in company day 4

--- client/components/signup/signup.js
+++ client/components/signup/signup.js
@@ -1,18 +1,24 @@
 import React from "react"
-
 import SignUpForm from "./../signupform/signup-form"
+import { connect } from "react-redux"
+import { userSignupRequest }  from './../../action/signup/signup-action'
+ import PropTypes from 'prop-types';
 class SignUp extends React.Component {
   render() {
+    const { userSignupRequest } = this.props;
     return (
       <div >
-        <h1> This is sign up page </h1>        
+        <h1> This is sign up page </h1>
       	<div className="row">
       		<div className="col-md-4 col-md-offset-4">
-      			<SignUpForm />
+      			<SignUpForm userSignupRequest={userSignupRequest}/>
       		</div>
       	</div>
      </div>
     )
   }
 }
-export default SignUp
+SignUp.propTypes = {
+  userSignupRequest: PropTypes.func.isRequired,
+}
+export default connect(null, {userSignupRequest})(SignUp);
--- client/views/routes.js
+++ client/views/routes.js
@@ -3,30 +3,40 @@ import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import App from './Layout';
 import SignUp from './../components/signup/signup'
 import Greeting from './../components/Greetings/Greetings'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+
 class Routes extends React.Component {
   render() {
+    const store = createStore(
+      (state = {}) => state,
+      applyMiddleware(thunk)
+    );
     return (
-      <Router>
-        <div>
+      <Provider store = {store}>
+        <Router>
           <div>
-            <nav className="navbar navbar-default">
-              <div className="container-fluid">
-                <ul className="nav navbar-nav">
-                  <li><Link to='/'>Home</Link></li> 
-                </ul>
-                <ul className="nav navbar-nav pull-right">
-                  <li><Link to='/signup' className="pull-right">Sign Up</Link></li>
-                </ul>
-              </div>
-            </nav>
+            <div>
+              <nav className="navbar navbar-default">
+                <div className="container-fluid">
+                  <ul className="nav navbar-nav">
+                    <li><Link to='/'>Home</Link></li>
+                  </ul>
+                  <ul className="nav navbar-nav pull-right">
+                    <li><Link to='/signup' className="pull-right">Sign Up</Link></li>
+                  </ul>
+                </div>
+              </nav>
+            </div>
+            <Switch>
+              <Route path='/' exact component={App} />
+              <Route path='/signup'  component={SignUp} />
+              <Route path='/greeting'  component={SignUp} />
+            </Switch>
           </div>
-          <Switch>
-            <Route path='/' exact component={App} />
-            <Route path='/signup'  component={SignUp} />
-            <Route path='/greeting'  component={SignUp} />
-          </Switch>        
-        </div>
-      </Router>
+        </Router>
+      </Provider>
     )
   }
 }
